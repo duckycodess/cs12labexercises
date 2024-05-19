c4bfb07bfe2319e9688a71f816f2b3ee0dfc9a7b
Tristan Jovan Noval <138286670+duckycodess@users.noreply.github.com>
2024-05-19 17:02:40 +0800

    Update cs12232lab09f.py

--- cs12232lab09/cs12232lab09f.py
+++ cs12232lab09/cs12232lab09f.py
@@ -1,7 +1,6 @@
 def powerSet(s: str, i: int, curr: str) -> list[str]: 
     return [curr] if i == len(s) else powerSet(s, i + 1, curr + s[i]) + powerSet(s, i + 1, curr)
 
-
 def count_k(p: list[str], k: int) -> list[str]:
     return [] if not p else ([p[0]] + count_k(p[1:], k) if len(p[0]) == k else count_k(p[1:], k))
 
