764b182895b301c6b14425c7e8ae028ac19fde4e
Tristan Jovan Noval <138286670+duckycodess@users.noreply.github.com>
2024-06-08 16:29:02 +0800

    Update cs12232lab11c.py

--- cs12232lab11/cs12232lab11c.py
+++ cs12232lab11/cs12232lab11c.py
@@ -9,38 +9,3 @@ def remove_none(child: tuple[Branch[T] | None, ...]) -> tuple[Branch[T], ...]:
 def trim(is_bad: Callable[[T], bool], branch: Branch[T]) -> Branch[T] | None:
     return None if is_bad(branch.value) else \
         Branch(branch.value, remove_none(tuple(trim(is_bad, child) for child in branch.children)))
-
-        
-
-tree = Branch(value=69, children=(
-        Branch(value=80, children=(
-            Branch(value=80),
-            Branch(value=10),
-            Branch(value=69),
-        )),
-        Branch(value=20),
-        Branch(value=70),
-        Branch(value=120, children=(
-            Branch(value=60),
-        )),
-    ))
-
-
-def is_bad1(value: int) -> bool:
-    return not (50 <= value <= 100)
-print(
-    trim(is_bad1, tree)
-)
-
-assert trim(is_bad1, tree) == Branch(value=69, children=(
-    Branch(value=80, children=(
-        Branch(value=80),
-        Branch(value=69),
-    )),
-    Branch(value=70),
-))
-
-def is_bad2(value: int) -> bool:
-    return value == 69
-
-assert trim(is_bad2, tree) is None
