8b3874cce41dd7ef64f3bbd601a1cc19884576ed
Tristan Jovan Noval <138286670+duckycodess@users.noreply.github.com>
2024-07-26 17:48:19 +0800

    Update hope3d4.py

--- hope3/hope3d4.py
+++ hope3/hope3d4.py
@@ -1,4 +1,3 @@
-import time
 def count_repdigit_decompositions(n: int) -> int:
     rep = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999, 
            1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 11111, 22222, 33333, 44444, 55555, 66666, 77777]
@@ -12,14 +11,11 @@ def count_repdigit_decompositions(n: int) -> int:
             
     return dp[-1]
 
-start = time.time()
-count_repdigit_decompositions(50000)
-end = time.time()
-print(end-start)
 
 def test_count_repdigit_decompositions():
     print(count_repdigit_decompositions(4) == 5)
     print(count_repdigit_decompositions(20) == 518)
     print(count_repdigit_decompositions(50) == 62659)
     print(count_repdigit_decompositions(100) == 7663645)
+test_count_repdigit_decompositions()
 
